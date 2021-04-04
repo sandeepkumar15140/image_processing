415a15009274aab84639c71846fcc257f25f5b9b
Sandeep <sandeep.kumar15140@gmail.com>
2021-04-04 20:31:27 +0530

    Uploading the project

---
+++ image_processing.py
@@ -0,0 +1,23 @@
+
+#https://pillow.readthedocs.io/en/stable/
+from PIL import Image, ImageFilter
+
+img = Image.open('./pokedox/pikachu.jpg')
+
+#to blur the image
+blur_img = img.filter(ImageFilter.BLUR) 
+blur_img.save("sharp.png", 'png')
+
+#to blur the image
+sharpen_img = img.filter(ImageFilter.SHARPEN)
+sharpen_img.save("sharp.png", 'png')
+
+#to grey the image
+grey_img = img.convert('L')
+grey_img.save('grey.png', 'png')
+#grey_img.show() ---to show the image
+
+#to roate the image
+rotate_img = img.rotate(90) #90 degree rotate
+rotate_img.save("rotated.png", 'png')
+rotate_img.show()
